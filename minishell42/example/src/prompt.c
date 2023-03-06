6fe2d79dc34f3d1e2c40b11aac070d9987698470
Jean-baptiste DE LA FAGE <jde-la-f@e2r2p8.clusters.42paris.fr>
2023-03-06 17:17:44 +0100

    add minishell

---
+++ minishell42/example/src/prompt.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prompt.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperez-b <aperez-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/10 17:02:33 by aperez-b          #+#    #+#             */
+/*   Updated: 2022/03/14 16:19:23 by aperez-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+extern int	g_status;
+
+static char	*get_home(t_prompt prompt)
+{
+	char	*temp;
+	char	*pwd;
+	char	*home;
+
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+		pwd = ft_strdup("∅ ");
+	home = mini_getenv("HOME", prompt.envp, 4);
+	if (home && home[0] && ft_strnstr(pwd, home, ft_strlen(pwd)))
+	{
+		temp = pwd;
+		pwd = ft_strjoin("~", &pwd[ft_strlen(home)]);
+		free(temp);
+	}
+	free(home);
+	home = ft_strjoin(BLUE, pwd);
+	free(pwd);
+	pwd = ft_strjoin(home, " ");
+	free(home);
+	home = ft_strjoin(" ", pwd);
+	free(pwd);
+	pwd = ft_strjoin(home, DEFAULT);
+	free(home);
+	return (pwd);
+}
+
+static char	*get_user(t_prompt prompt)
+{
+	char	**user;
+	char	*temp;
+	char	*temp2;
+
+	user = NULL;
+	temp2 = NULL;
+	exec_custom(&user, "/usr/bin/whoami", "whoami", prompt.envp);
+	if (!user)
+		user = ft_extend_matrix(user, "guest");
+	if (!ft_strncmp(user[0], "root", 4))
+		temp2 = ft_strjoin(NULL, RED);
+	else if ((int)(user[0][0]) % 5 == 0)
+		temp2 = ft_strjoin(NULL, CYAN);
+	else if ((int)(user[0][0]) % 5 == 1)
+		temp2 = ft_strjoin(NULL, GRAY);
+	else if ((int)(user[0][0]) % 5 == 2)
+		temp2 = ft_strjoin(NULL, GREEN);
+	else if ((int)(user[0][0]) % 5 == 3)
+		temp2 = ft_strjoin(NULL, MAGENTA);
+	else
+		temp2 = ft_strjoin(NULL, YELLOW);
+	temp = ft_strjoin(temp2, *user);
+	free(temp2);
+	ft_free_matrix(&user);
+	return (temp);
+}
+
+char	*mini_getprompt(t_prompt prompt)
+{
+	char	*temp;
+	char	*temp2;
+	char	*aux;
+
+	temp = get_user(prompt);
+	temp2 = ft_strjoin(temp, "@minishell");
+	free(temp);
+	aux = get_home(prompt);
+	temp = ft_strjoin(temp2, aux);
+	free(aux);
+	free(temp2);
+	if (!g_status || g_status == -1)
+		temp2 = ft_strjoin(temp, BLUE);
+	else
+		temp2 = ft_strjoin(temp, RED);
+	free(temp);
+	temp = ft_strjoin(temp2, "$ ");
+	free(temp2);
+	temp2 = ft_strjoin(temp, DEFAULT);
+	free(temp);
+	return (temp2);
+}
